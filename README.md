a333132117954965e61f2cb185637ec5f98ec4fc
Deykun <stdeykun+git@gmail.com>
2017-05-07 00:59:29 +0200

    Wersja 1.10

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Dodaje guzik który umożliwia przewiniecie listy wpisów do kolejnego znajdują
 
 Kiedy rozwiniemy 100 komentarzy do wybranego wpisu i zrezygnujemy z czytania po 10 wystarczy kliknąć guzik by przewinąć niepotrzebne 90.
 
+[Skrypt na stornie Greasyfork](https://greasyfork.org/en/scripts/29515-przewijanie-mikrobloga).
+
 ![](http://x3.wykop.pl/cdn/c3201142/comment_vdowv7cxnUbGPtRONBbUUiSUAZzMgPUA.gif)
+
+#### Wersja 1.10
+- Przewijanie wpisów klawiszami N (następny) i B (poprzedni)
+- Inne poprawki
--- przewijanie.user.js
+++ przewijanie.user.js
@@ -1,44 +1,55 @@
 // ==UserScript==
 // @name        Przewijanie mikrobloga
-// @description Dodaje możliwość przewjania wpisów.
-// @version     1.00
+// @namespace   http://www.wykop.pl/ludzie/Deykun
+// @description Dodaje możliwość przewjania wpisów na mikroblogu. Klawisze B i N przewijają stronę.
+// @author      Deykun
+// @version     1.10
 // @include     htt*.wykop.pl/mikroblog/*
 // @grant       none
 // @run-at			document-end
 // ==/UserScript==
 
 $(document).ready(function () {
-  var $entry = $('div[data-type="entry"][data-id]:not(.media-content)'),
+  var $elements = $('#commentForm, div[data-type="entry"][data-id]:not(.media-content), .pager'),
       navHeight = $('#nav').height(),
       bodyHeight = $('body').height(),
-      entryPos = [];
+      elemPos = [];
 
   $('body').append('<div style="position:fixed; width: 30px; height:30px; left: 0; top:'+(navHeight+70)+'px; left:5px; z-index:10;"> <button id="next"><i class="fa fa-chevron-down"></i></button></div>');
 
   function getY() {
-    entryPos = [];
+    elemPos = [];
     navHeight = $('#nav').height();
     bodyHeight = $('body').height();
 
-    $entry.each( function() {
-      entryPos.push($(this).offset().top);
+    $elements.each( function() {
+      elemPos.push($(this).offset().top);
     });
+
 
     $('#next').css('top', (navHeight+60));
   };
   getY();
 
 
-  $('#next').on('click', function (e) {
+  function nextEntry(direction) {
     // Strona doładywuje wpisy po pewnym czasie
     if (bodyHeight !== $('body').height()) { getY(); }
 
     var y = window.pageYOffset;
 
-    for (var i = 1, imax = entryPos.length; i < imax ; i++) {
-      if (entryPos[i] > y+navHeight+1) {
-        var distanceScroll = $entry.eq(i).offset().top-navHeight,
-            timeScroll = 800,
+    for (var i = 1, imax = elemPos.length; i < imax ; i++) {
+      if (elemPos[i] > y+navHeight+1) {
+        var distanceScroll = $elements.eq(i).offset().top-navHeight;
+        if (direction === 'before') {
+          if (i !== 0 && i !== 1) {
+            distanceScroll = $elements.eq(i-2).offset().top-navHeight;
+          } else {
+            distanceScroll = y;
+          }
+        }
+
+        var timeScroll = 800,
             diffScroll = (y-distanceScroll);
 
         if (diffScroll < 500) { timeScroll = 400; }
@@ -50,5 +61,21 @@ $(document).ready(function () {
         break;
       }
     }
+  }
+
+  $(document).on('keyup', (e)=> {
+      var ktrigger = e.target.tagName.toLowerCase();
+      if (ktrigger != 'input' && ktrigger != 'textarea') {
+        switch (e.which) {
+          case 78:
+            nextEntry();
+            break;
+          case 66:
+            nextEntry('before');
+            break;
+        }
+      }
   });
+
+  $('#next').on('click', nextEntry);
 });
