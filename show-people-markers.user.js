a4755fbae1583555af49e9aef212868635877dbc
Szymon Tondowski <stdeykun+git@gmail.com>
2022-09-18 00:32:50 +0200

    Marker for blocked people

--- show-people-markers.user.js
+++ show-people-markers.user.js
@@ -36,7 +36,7 @@ const debounce = (fn, time) => {
 }
 
 // Stolen from https://userscripts-mirror.org/scripts/show/117457
-const fetchPeople = (nick, type) => {
+const fetchPeopleByFollowStatus = (nick, type) => {
     let page = 1;
     let end = false;
     const people = [];
@@ -65,19 +65,44 @@ const fetchPeople = (nick, type) => {
     return people;
 }
 
+const fetchPeopleFromBlackList = () => {
+  const people = [];
+
+  $.ajax({
+      method: 'GET',
+      async: false,
+      url: '/ustawienia/czarne-listy/',
+      success: (html) => {
+          if ($('[data-type="users"] a b', html).length) {
+              $('[data-type="users"] a b', html).each(function () {
+                  people.push($(this).text().trim());
+              });
+              if ((people.length % 75) > 0) end = true;
+          } else end = true;
+      },
+      error: function (xhr) {
+          console.log(xhr.status + ' ' + xhr.statusText);
+          end = true;
+      }
+  });
+
+  return people;
+}
+
 const generateCache = (username) => {
-
     const currentTimestamp = (new Date()).getTime();
-        const followers = fetchPeople(username, 'followers');
-    const followed = fetchPeople(username, 'followed');
+    const followers = fetchPeopleByFollowStatus(username, 'followers');
+    const followed = fetchPeopleByFollowStatus(username, 'followed');
+    const blacklisted = fetchPeopleFromBlackList();
 
-const newCache = {
-    lastUpdate: currentTimestamp,
-    followers,
-    followed,
-};
+    const newCache = {
+        lastUpdate: currentTimestamp,
+        followers,
+        followed,
+        blacklisted,
+    };
 
-localStorage.setItem('spm-cache', JSON.stringify(newCache));
+    localStorage.setItem('spm-cache', JSON.stringify(newCache));
 
     return newCache;
 }
@@ -99,12 +124,14 @@ const getPeople = (username) => {
         }
     }
 
-return generateCache();
-
+  return generateCache(username);
 }
 
-const setMarkers = ({     followers,
-    followed }) => {
+const setMarkers = ({
+  followers = [],
+  followed = [],
+  blacklisted = [],
+} = {}) => {
 
     const selectors = [
       '.voters-list .link', // plusy na mikroblogu
@@ -116,6 +143,7 @@ const setMarkers = ({     followers,
 
         const isFollower = followers.includes(username);
         const isFollowed = followed.includes(username);
+        const isBlacklisted = blacklisted.includes(username);
 
         if (isFollower) {
           if (!el.classList.contains('spm-follower')) {
@@ -126,6 +154,12 @@ const setMarkers = ({     followers,
         if (isFollowed) {
           if (!el.classList.contains('spm-followed')) {
             el.classList.add('spm-followed');
+          }
+        }
+
+        if (isBlacklisted) {
+          if (!el.classList.contains('spm-blacklisted')) {
+            el.classList.add('spm-blacklisted');
           }
         }
     });
@@ -145,58 +179,83 @@ domReady(() => {
     }
 
     appendCSS(`
+      .votersContainer .voters-list {
+        text-align: left;
+      }
+
       .spm-followed::after,
-      .spm-follower::after {
-        content: '✓';
+      .spm-follower::after,
+      .spm-blacklisted::after {
+        content: '✔';
         display: inline-block;
-        text-align: center;
         border-radius: 15px;
         margin-left: 3px;
         border: 1px solid #06b206;
-        color: white;
-        padding: 0 3px;
-        min-width: 15px;
-        max-width: 15px;
+        padding: 0 4px;
+        min-width: 16px;
+        max-width: 16px;
         height: 15px;
         vertical-align: sub;
         line-height: 13px;
         font-size: 9px;
-        transition: .3s ease-in-out;
+        font-weight: 400;
+        transition: .1s ease-in-out;
         white-space: nowrap;
         overflow: hidden;
-        text-overflow: ellipsis;
       }
 
       .spm-followed:hover::after,
-      .spm-follower:hover::after {
+      .spm-follower:hover::after,
+      .spm-blacklisted:hover::after {
         max-width: 100px;
       }
 
+      .spm-blacklisted:hover::after {
+        content: '✘ blokujesz';
+      }
+
       .spm-follower:hover::after {
-        content: '✓ obserwujący';
+        content: '✔ obserwuje';
       }
 
       .spm-followed:hover::after {
-        content: '✓ obserwowany';
+        content: '✔ obserwujesz';
       }
 
       .spm-follower.spm-followed:hover::after {
-        content: '✓ obserwujący/any';
+        content: '✔ obserwuje (i -sz)';
+      }
+
+      .spm-blacklisted::after {
+        content: '✘';
+        background-color: #b0adad;
+        border-color: #b0adad;
+        color: #602121;
       }
       
       .spm-follower::after {
-        color: #06b206;
+        color: black;
+        border-color: #91dd91;
+        background-color: #91dd91;
       }
 
       .spm-followed::after {
         color: white;
+        border-color: #06b206;
         background-color: #06b206;
+      }
+
+      .spm-follower.spm-followed::after {
+        border-color: #067a06;
+        background-color: #067a06;
       }
     `);
 
     const username = document.querySelector('.logged-user .avatar').getAttribute('alt');
 
     const people = getPeople(username);
+
+    console.log(people);
 
     setMarkers(people);
 
